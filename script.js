0ad5ef2b696e4f643837ca0b991dc97d4a2166e4
Klinger Cardoso Marinho <p001556@prodam.local>
2024-01-05 16:33:43 -0400

    reco voz v1

---
+++ script.js
@@ -0,0 +1,33 @@
+const button =  document.querySelector('button')
+const paragrafo = document.querySelector('p')
+const recognition = createRecognition()
+let listening = false
+
+button.addEventListener('click', e => {
+    if(!recognition) return
+    listening ? recognition.stop() : recognition.start()
+    button.innerHTML =  listening ? 'Aperte para falar' : 'Parar de escutar'
+    button.innerHTML.toggle('TESTE') 
+    button.innerHTML.toggle('TESTE1') 
+})
+
+
+function createRecognition () {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    const recognition =  SpeechRecognition !== undefined ? new SpeechRecognition() : null
+
+    if (!recognition) {
+        paragrafo.innerHTML = ' Funcao nao encontrado'
+        return null
+    }
+recognition.lang = "pt_BR"
+recognition.onstart = () => listening = true
+recognition.onend = () => listening = false
+recognition.onerror = e => console.log('error',e)
+recognition.onresult = e => paragrafo.innerHTML = e.results[0][0].transcript
+return recognition
+
+}
+
+
+
